2e713eab64b94e051bfc72569b47242f69f6618c
Arsonull <54869595+arsonull@users.noreply.github.com>
2020-03-07 13:49:47 -0700

    Milestone 4 Done

---
+++ Milestone/4/MineSweeperGUI/MineSweeperGUI/Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeperGUI
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            int count = 1;
+
+            int sX = 10;
+            int sY = 10;
+            int width = 40;
+            int height = 40;
+            int dist = 20;
+
+            for (int r = 0; r < 10; r++)
+            {
+                for (int c = 0; c < 10; c++)
+                {
+                    Button butt = new Button();
+                    butt.Name = Convert.ToString(count);
+                    butt.Top = sX + (r * height + dist);
+                    butt.Left = sY + (c * height + dist);
+                    butt.Width = width; 
+                    butt.Height = height;
+                    butt.Click += MyButtonClick;
+                    this.Controls.Add(butt);
+                    count++;
+                }
+            }
+        }
+
+        void MyButtonClick(object sender, EventArgs e)
+        {
+            Button butt = sender as Button;
+
+            butt.Text = butt.Name;
+        }
+    }
+}
